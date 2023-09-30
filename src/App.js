1785a459f513d7fc599ce64a042cdc256760c9a2
Akhil <akhil.donthula8977@gmail.com>
2023-09-30 23:32:29 +0530

    product pagee

--- src/App.js
+++ src/App.js
@@ -2,12 +2,16 @@
 import './App.css';
 import NavigationBar from './Components/Navigationbar/Navbar';
 
-
+import Productspage from './Screens/Productspage/Productspage';
 function App() {
   return (
+    <>
     <div className="App">
    <NavigationBar/>
     </div>
+    <Productspage/>
+    </>
+    
   );
 }
 
